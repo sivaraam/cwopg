7c9653318f703c4c771666aaae18e722c690e8b5
Kaartic Sivaraam <kaartic.sivaraam@gmail.com>
2018-04-02 20:18:56 +0530

    Cleanup: avoid polluting the global scope
    ... using the "wrapping all statements into an unnamed function and
    calling it immediately" idiom.

--- article_list_generator/lib/index.js
+++ article_list_generator/lib/index.js
@@ -5,136 +5,138 @@
   * categories or it's children.
   */
 
-/**
-  * The library required for manipulating HTTP GET requests
-  * and it's response.
-  */
-const request = require('request');
-const path = require('path');
-const fs = require('fs');
-const e = require('../../lib/error.js');
+(function () {
+	/**
+	  * The library required for manipulating HTTP GET requests
+	  * and it's response.
+	  */
+	const request = require('request');
+	const path = require('path');
+	const fs = require('fs');
+	const e = require('../../lib/error.js');
 
-const petscan_url = 'https://petscan.wmflabs.org/';
+	const petscan_url = 'https://petscan.wmflabs.org/';
 
-/**
-  * Get the PetScan parameters that don't change often.
-  */
-const petscan_params = require('../config/article_list_generator.json');
+	/**
+	  * Get the PetScan parameters that don't change often.
+	  */
+	const petscan_params = require('../config/article_list_generator.json');
 
-/**
-  * Reads the category list file and returns the list of categories in it.
-  *
-  * Returns an array in which each element represents a category. Returns null
-  * in case the file doesn't exist.
-  */
-const read_category_list_file = function (category_list_file) {
-	// ensure that the category list file exists
-	try {
-		fs.statSync(category_list_file);
-	} catch(error) {
-		if(error.code === 'ENOENT') {
+	/**
+	  * Reads the category list file and returns the list of categories in it.
+	  *
+	  * Returns an array in which each element represents a category. Returns null
+	  * in case the file doesn't exist.
+	  */
+	const read_category_list_file = function (category_list_file) {
+		// ensure that the category list file exists
+		try {
+			fs.statSync(category_list_file);
+		} catch(error) {
+			if(error.code === 'ENOENT') {
+				return null;
+			}
+
+			throw error;
+		}
+
+		console.log(`About to read the category list file (${category_list_file}).`);
+
+		const categories = fs.readFileSync(category_list_file, 'utf-8')
+				     .split('\n')
+				     .filter(Boolean);  // to ignore empty lines
+
+		// sanity check
+		if (categories == null) {
 			return null;
 		}
 
-		throw error;
-	}
+		console.log(`Successfully read ${categories.length} categories.`);
 
-	console.log(`About to read the category list file (${category_list_file}).`);
+		return categories;
+	};
 
-	const categories = fs.readFileSync(category_list_file, 'utf-8')
-			     .split('\n')
-			     .filter(Boolean);  // to ignore empty lines
+	/**
+	  * Writes the given set of articles to the given article list file.
+	  */
+	const write_article_list_file = function (articles, article_list_file) {
+		console.log(`About to generate the article list file (${article_list_file}).`);
 
-	// sanity check
-	if (categories == null) {
-		return null;
-	}
+		fs.writeFileSync(article_list_file, articles.join('\n'),  function(err) {
+			if(err) {
+				throw err;
+			}
 
-	console.log(`Successfully read ${categories.length} categories.`);
+			console.log('Generated the article list file.');
+		});
+	};
 
-	return categories;
-};
+	/**
+	  * Generates the article list from the PetScan's response which is given in as
+	  * a JSON string and writes one article per line in the given artile list file.
+	  */
+	const generate_article_list_file_from_petscan_res = function (petscan_json_response, article_list_file) {
+		const petscan_response = JSON.parse(petscan_json_response);
+		const articles_object = petscan_response['*'][0].a['*'];
 
-/**
-  * Writes the given set of articles to the given article list file.
-  */
-const write_article_list_file = function (articles, article_list_file) {
-	console.log(`About to generate the article list file (${article_list_file}).`);
-
-	fs.writeFileSync(article_list_file, articles.join('\n'),  function(err) {
-		if(err) {
-			throw err;
+		if (articles_object === null) {
+			e.fatal_error('Failed to fetch article list from PetScan response');
 		}
 
-		console.log('Generated the article list file.');
-	});
-};
+		const articles = [];
 
-/**
-  * Generates the article list from the PetScan's response which is given in as
-  * a JSON string and writes one article per line in the given artile list file.
-  */
-const generate_article_list_file_from_petscan_res = function (petscan_json_response, article_list_file) {
-	const petscan_response = JSON.parse(petscan_json_response);
-	const articles_object = petscan_response['*'][0].a['*'];
-
-	if (articles_object === null) {
-		e.fatal_error('Failed to fetch article list from PetScan response');
-	}
-
-	const articles = [];
-
-	for (var curr=0; curr<articles_object.length; curr++) {
-		articles.push(articles_object[curr].title);
-	}
-
-	if (articles.length === 0) {
-		e.fatal_error('No articles found for the given category.');
-	}
-
-	console.log(`Successfully got ${articles.length} articles for the given set of categories.`);
-
-	write_article_list_file(articles, article_list_file);
-};
-
-const generate_article_list = function (category_list_file_name, article_list_file_name) {
-
-	/* Resolve the name of the files to their corresponding relative path */
-	const project_root = path.dirname(require.main.filename);
-	const category_list_file = path.resolve(project_root, category_list_file_name);
-	const article_list_file = path.resolve(project_root, article_list_file_name);
-
-	const categories = read_category_list_file(category_list_file);
-
-	if (categories === null) {
-		e.fatal_error(`Category list file ${category_list_file} does not exist`);
-	}
-
-	if (categories.length === 0) {
-		e.fatal_error('No categories found.');
-	}
-
-	petscan_params.categories = categories.join('\n');
-
-	console.log('Requesting the article list for the given set of categories.');
-
-	// Do the HTTP request to PetScan to get the list of articles for the given
-	// set of categories.
-	request.get({
-		url: petscan_url,
-		qs: petscan_params
-	}, function (error, response, body) {
-		if (error != null) {
-			e.fatal_error(`error: ${error}`);
+		for (var curr=0; curr<articles_object.length; curr++) {
+			articles.push(articles_object[curr].title);
 		}
-		if (response && response.statusCode === 200) {
-			generate_article_list_file_from_petscan_res(body, article_list_file);
-		} else if (!response) {
-			e.fatal_error('No response received from PetScan!');
-		} else {
-			e.fatal_error(`PetScan request failed with status code: ${response.statusCode}\n${response.body}`);
-		}
-	});
-};
 
-module.exports.generate_article_list = generate_article_list;
+		if (articles.length === 0) {
+			e.fatal_error('No articles found for the given category.');
+		}
+
+		console.log(`Successfully got ${articles.length} articles for the given set of categories.`);
+
+		write_article_list_file(articles, article_list_file);
+	};
+
+	const generate_article_list = function (category_list_file_name, article_list_file_name) {
+
+		/* Resolve the name of the files to their corresponding relative path */
+		const project_root = path.dirname(require.main.filename);
+		const category_list_file = path.resolve(project_root, category_list_file_name);
+		const article_list_file = path.resolve(project_root, article_list_file_name);
+
+		const categories = read_category_list_file(category_list_file);
+
+		if (categories === null) {
+			e.fatal_error(`Category list file ${category_list_file} does not exist`);
+		}
+
+		if (categories.length === 0) {
+			e.fatal_error('No categories found.');
+		}
+
+		petscan_params.categories = categories.join('\n');
+
+		console.log('Requesting the article list for the given set of categories.');
+
+		// Do the HTTP request to PetScan to get the list of articles for the given
+		// set of categories.
+		request.get({
+			url: petscan_url,
+			qs: petscan_params
+		}, function (error, response, body) {
+			if (error != null) {
+				e.fatal_error(`error: ${error}`);
+			}
+			if (response && response.statusCode === 200) {
+				generate_article_list_file_from_petscan_res(body, article_list_file);
+			} else if (!response) {
+				e.fatal_error('No response received from PetScan!');
+			} else {
+				e.fatal_error(`PetScan request failed with status code: ${response.statusCode}\n${response.body}`);
+			}
+		});
+	};
+
+	module.exports.generate_article_list = generate_article_list;
+})();
--- category_list_generator/lib/index.js
+++ category_list_generator/lib/index.js
@@ -5,127 +5,129 @@
  * input.
  */
 
-const fs = require('fs');
-const path = require('path');
-const e = require('../../lib/error.js');
-const preprocess = require('../../preprocess/lib/preprocess');
+(function () {
+	const fs = require('fs');
+	const path = require('path');
+	const e = require('../../lib/error.js');
+	const preprocess = require('../../preprocess/lib/preprocess');
 
-/**
- * Generates an array representation of the list of enwiki categories in the file.
- * Each line is expected to represent a valid category in enwiki.
- *
- * Returns the array of categoreis on success. Returns null in case of an error.
- */
-const read_enwiki_cats = function (enwiki_cats_file) {
-	// ensure that the enwiki category list file exists
-	try {
-		fs.statSync(enwiki_cats_file);
-	} catch(error) {
-		if(error.code === 'ENOENT') {
-			console.error(error);
+	/**
+	 * Generates an array representation of the list of enwiki categories in the file.
+	 * Each line is expected to represent a valid category in enwiki.
+	 *
+	 * Returns the array of categoreis on success. Returns null in case of an error.
+	 */
+	const read_enwiki_cats = function (enwiki_cats_file) {
+		// ensure that the enwiki category list file exists
+		try {
+			fs.statSync(enwiki_cats_file);
+		} catch(error) {
+			if(error.code === 'ENOENT') {
+				console.error(error);
+				return null;
+			}
+
+			throw error;
+		}
+
+		console.log(`About to read the enwiki category list file (${enwiki_cats_file}).`);
+
+		const enwiki_cats = fs.readFileSync(enwiki_cats_file, 'utf-8')
+				      .split('\n')
+				      .filter(Boolean);  // to ignore empty lines
+
+		// sanity check
+		if (enwiki_cats == null) {
 			return null;
 		}
 
-		throw error;
-	}
+		console.log(`Successfully read ${enwiki_cats.length} categories.`);
 
-	console.log(`About to read the enwiki category list file (${enwiki_cats_file}).`);
-
-	const enwiki_cats = fs.readFileSync(enwiki_cats_file, 'utf-8')
-			      .split('\n')
-			      .filter(Boolean);  // to ignore empty lines
-
-	// sanity check
-	if (enwiki_cats == null) {
-		return null;
-	}
-
-	console.log(`Successfully read ${enwiki_cats.length} categories.`);
-
-	return enwiki_cats;
-};
-
-/**
- * Returns true if the category given as a string is relevant to the given
- * user query string.
- *
- * For now, a category is relevant if the there is a sub-word taht matches with
- * the given user query string.
- */
-const is_cat_relevant = function (cat, user_query_elems) {
-	var relevant = false;
-
-	cat.forEach (function (cat_elem) {
-		if (user_query_elems.includes (cat_elem))
-			relevant = true;
-	});
-
-	return relevant;
-}
-
-/**
- * Finds a list of relevant categories from the given set of enwiki categories
- * for the given user query string.
- */
-const get_relevant_cats = function (user_query, enwiki_cats_arr) {
-	const relevant_cats = [];
-	const user_query_elems = preprocess (user_query);
-	const enwiki_cats_elems = [];
-
-	// preprocess the Categories
-	enwiki_cats_arr.forEach (function (cat) {
-		enwiki_cats_elems.push(preprocess (cat));
-	});
-
-	for (var i=0; i < enwiki_cats_arr.length; i++) {
-		if (is_cat_relevant (enwiki_cats_elems[i], user_query_elems)) {
-			relevant_cats.push (enwiki_cats_arr[i]);
-		}
-	}
-
-	console.log (`Found ${relevant_cats.length} categories for the user query '${user_query}'`);
-	return relevant_cats;
-}
-
-/**
- * Generate the category list file consisting of relevant categories for a
- * given user query string.
- */
-const generate_category_list = function (user_query, enwiki_cats_file_name, category_list_file_name) {
-
-	/* Resolve the name of the files to their corresponding relative path */
-	const project_root = path.dirname(require.main.filename);
-	const enwiki_cats_file = path.resolve(project_root, enwiki_cats_file_name);
-	const category_list_file = path.resolve(project_root, category_list_file_name);
+		return enwiki_cats;
+	};
 
 	/**
-	 * The array corresponding to the category list in the file.
+	 * Returns true if the category given as a string is relevant to the given
+	 * user query string.
+	 *
+	 * For now, a category is relevant if the there is a sub-word taht matches with
+	 * the given user query string.
 	 */
-	const enwiki_cats_arr = read_enwiki_cats(enwiki_cats_file);
+	const is_cat_relevant = function (cat, user_query_elems) {
+		var relevant = false;
 
-	if (enwiki_cats_arr === null) {
-		e.fatal_error (`Error while reading enwiki categories file '${enwiki_cats_file}`);
+		cat.forEach (function (cat_elem) {
+			if (user_query_elems.includes (cat_elem))
+				relevant = true;
+		});
+
+		return relevant;
 	}
 
-	if (enwiki_cats_arr.length === 0) {
-		e.fatal_error (`Could not get any categories from the enwiki categories file '${enwiki_cats_file}'`);
-	}
+	/**
+	 * Finds a list of relevant categories from the given set of enwiki categories
+	 * for the given user query string.
+	 */
+	const get_relevant_cats = function (user_query, enwiki_cats_arr) {
+		const relevant_cats = [];
+		const user_query_elems = preprocess (user_query);
+		const enwiki_cats_elems = [];
 
-	const relevant_cats = get_relevant_cats (user_query, enwiki_cats_arr);
+		// preprocess the Categories
+		enwiki_cats_arr.forEach (function (cat) {
+			enwiki_cats_elems.push(preprocess (cat));
+		});
 
-	if (relevant_cats.length === 0) {
-		e.fatal_error ('Could not find any relevant categories for the given query');
-	}
-
-	console.log(`About to generate the category list file (${category_list_file}).`);
-
-	fs.writeFileSync(category_list_file, relevant_cats.join('\n'), function(err) {
-		if(err) {
-			throw err;
+		for (var i=0; i < enwiki_cats_arr.length; i++) {
+			if (is_cat_relevant (enwiki_cats_elems[i], user_query_elems)) {
+				relevant_cats.push (enwiki_cats_arr[i]);
+			}
 		}
 
-		console.log('Generated the category list file.');
-	});
-};
+		console.log (`Found ${relevant_cats.length} categories for the user query '${user_query}'`);
+		return relevant_cats;
+	}
 
-module.exports.generate_category_list = generate_category_list;
+	/**
+	 * Generate the category list file consisting of relevant categories for a
+	 * given user query string.
+	 */
+	const generate_category_list = function (user_query, enwiki_cats_file_name, category_list_file_name) {
+
+		/* Resolve the name of the files to their corresponding relative path */
+		const project_root = path.dirname(require.main.filename);
+		const enwiki_cats_file = path.resolve(project_root, enwiki_cats_file_name);
+		const category_list_file = path.resolve(project_root, category_list_file_name);
+
+		/**
+		 * The array corresponding to the category list in the file.
+		 */
+		const enwiki_cats_arr = read_enwiki_cats(enwiki_cats_file);
+
+		if (enwiki_cats_arr === null) {
+			e.fatal_error (`Error while reading enwiki categories file '${enwiki_cats_file}`);
+		}
+
+		if (enwiki_cats_arr.length === 0) {
+			e.fatal_error (`Could not get any categories from the enwiki categories file '${enwiki_cats_file}'`);
+		}
+
+		const relevant_cats = get_relevant_cats (user_query, enwiki_cats_arr);
+
+		if (relevant_cats.length === 0) {
+			e.fatal_error ('Could not find any relevant categories for the given query');
+		}
+
+		console.log(`About to generate the category list file (${category_list_file}).`);
+
+		fs.writeFileSync(category_list_file, relevant_cats.join('\n'), function(err) {
+			if(err) {
+				throw err;
+			}
+
+			console.log('Generated the category list file.');
+		});
+	};
+
+	module.exports.generate_category_list = generate_category_list;
+})();
