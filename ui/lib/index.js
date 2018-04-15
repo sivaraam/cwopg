cd3d5c7fedaeaf77c7574522bac84b9ca22106f1
Kaartic Sivaraam <kaartic.sivaraam@gmail.com>
2018-04-15 15:45:10 +0530

    ui: request for generation of offline package using AJAX
    This is an attempt to see if the unfortunate "connection aborted"
    goes off.

--- ui/lib/index.js
+++ ui/lib/index.js
@@ -2,10 +2,12 @@
 
 const express = require ('express');
 const body_parser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const package_generator_orchestrator = require('../..');
 const path = require ('path');
 const app = express();
 const port = 3000;
+const file_path_cookie_id = 'cwopgZimFilePath';
 
 app.use( express.static( path.join(__dirname, '../static') ) );
 
@@ -24,6 +26,8 @@ app.use(
  */
 app.use(body_parser.json());
 
+app.use(cookieParser());
+
 app.get ('/', function (request, response) {
 	console.log (request.url);
 
@@ -37,7 +41,26 @@ app.get ('/', function (request, response) {
 	})
 });
 
-app.post ('/custom-enwiki-package.zim', function (request, response) {
+app.post ('/generate-package', function (request, response) {
+	const params = {
+		user_query: request.body.keywords,
+		nopic: request.body.nopic !== undefined,
+		novid: request.body.novid !== undefined
+	}
+
+	const package_callback = function (output_file) {
+		response.cookie(file_path_cookie_id, output_file, { httpOnly: true });
+		response.set('Cache-Control', 'max-age=60, must-revalidate');
+		response.set('Content-Type', 'text/plain');
+		response.status(200).send('Success!');
+	};
+
+	/* Generate the offline package for the obtained keywords */
+	package_generator_orchestrator.generate_package (params,
+	                                                 package_callback);
+});
+
+app.get ('/download-package', function (request, response) {
 	const download_options = {
 		dotfiles: 'deny',
 		headers: {
@@ -45,28 +68,21 @@ app.post ('/custom-enwiki-package.zim', function (request, response) {
 			'x-sent': true
 		}
 	};
+	const file_name = 'custom-enwiki-package.zim';
 
-	const package_callback = function (output_file) {
-		const file_name = 'custom-enwiki-package.zim';
+	if (request.cookies[file_path_cookie_id])
+	{
+		const file_path = request.cookies[file_path_cookie_id];
 
-		response.download (output_file, file_name, download_options, function (err) {
+		response.cookie(file_path_cookie_id, '');
+		response.download (file_path, file_name, download_options, function (err) {
 			if (err) {
 				console.log (err);
 			} else {
 				console.log ('Sent:', file_name);
 			}
 		});
-	};
-
-	const params = {
-		user_query: request.body.keywords,
-		nopic: request.body.nopic !== undefined,
-		novid: request.body.novid !== undefined
 	}
-
-	/* Generate the offline package for the obtained keywords */
-	package_generator_orchestrator.generate_package (params,
-	                                                 package_callback);
 });
 
 app.listen (port, function (err) {
